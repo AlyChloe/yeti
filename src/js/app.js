091aac18ff81014f7e6b20afbbb31a7df637d28b
Chloe Collier <achloecollier@gmail.com>
2016-12-13 21:29:15 -0500

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+/*********************************************************
+1. Install by copying all files EXCEPT node_modules folder.
+2. Then type in terminal: $ sudo npm install.
+3. $ grunt watch
+*********************************************************/
+(function($) {
+  "use strict";
+
+})(jQuery);
